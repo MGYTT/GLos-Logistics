f1b8ebf7cd0388ba40ef24dd02706cf632aa43fb
MGYTTTTT <mgytsklep@gmail.com>
2026-03-27 22:02:12 +0100

    update: Dodano speed-insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from '@/components/shared/Providers'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
